e242f920411dff7ad89bccb6acd44f4a223803d0
Max <pasemkomax@gmail.com>
2024-11-24 21:15:12 +0200

    2

--- src/main/java/com/ltp/javauniversitylab4/repository/ReserveRepository.java
+++ src/main/java/com/ltp/javauniversitylab4/repository/ReserveRepository.java
@@ -4,9 +4,11 @@ import com.ltp.javauniversitylab4.model.Reserve;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ReserveRepository extends JpaRepository<Reserve,Long> {
 
     Reserve findByHouse_HouseId(Long houseId);
 
+    Reserve findByClient_ClientId(Long clientId);
 }
--- src/main/java/com/ltp/javauniversitylab4/service/ReserveService.java
+++ src/main/java/com/ltp/javauniversitylab4/service/ReserveService.java
@@ -10,6 +10,7 @@ import com.ltp.javauniversitylab4.repository.HouseRepository;
 import com.ltp.javauniversitylab4.repository.ReserveRepository;
 import com.ltp.javauniversitylab4.utils.BookingException;
 import com.ltp.javauniversitylab4.utils.HouseAmenitie;
+import com.ltp.javauniversitylab4.utils.HouseHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,52 +30,34 @@ public class ReserveService {
     private ClientRepository clientRepository;
 
 
-
     public Reserve addOrUpdateReserve(ReserveRequestDto reserveRequestDto) {
         House house = houseRepository.findById(reserveRequestDto.getHouseId()).orElse(new House(-1L));
         Client client = clientRepository.findById(reserveRequestDto.getHouseId()).orElse(new Client(-1L));
+
         LocalDate localDate = reserveRequestDto.getDate();
 
-        Reserve reserve = new Reserve(house,client,localDate);
-
-        if (!houseRepository.existsById(house.getHouseId()) && !clientRepository.existsById(client.getClientId())) {
-            reserveRepository.save(reserve);
-
-            double priceForHouse = priceOfHouse(house);
-
-            if (localDate.getMonth() == Month.NOVEMBER || localDate.getMonth() == Month.MARCH) {
-                double discountInPercentage = 20;
-                double discount = discountInPercentage / 100;
-                double totalPriceForHouseWithDiscount = priceForHouse - priceForHouse * discount;
-
-                if (client.getBalance() >= totalPriceForHouseWithDiscount) {
-                    client.decrementBalance(totalPriceForHouseWithDiscount);
-                } else {
-                    throw new BookingException("Not enough money to pay.");
-                }
-            } else {
-                if (client.getBalance() >= priceForHouse) {
-                    client.decrementBalance(priceForHouse);
-                } else {
-                    throw new BookingException("Not enough money to pay.");
-                }
-            }
-            reserveRepository.save(reserve);
-        }
-        else if (clientRepository.existsById(client.getClientId())) {
-            throw new BookingException(client.getName() + " has already reserved house.");
-        } else {
+        Reserve checkHouseReserve = reserveRepository.findByHouse_HouseId(house.getHouseId());
+        Reserve checkClientReserve = reserveRepository.findByClient_ClientId(client.getClientId());
+        if (checkHouseReserve != null) {
             throw new BookingException("The house is already reserved.");
         }
+        if (checkClientReserve != null){
+            throw new BookingException("The client has already reserved the house.");
+        }
+        Reserve newReserve = new Reserve(house, client, localDate);
+        reserveRepository.save(newReserve);
 
-        return reserve;
+        Month month = localDate.getMonth();
+        double priceForHouse = HouseHelper.priceOfHouse(house, month);
+        if (client.getBalance() >= priceForHouse) {
+            client.decrementBalance(priceForHouse);
+        } else {
+            throw new BookingException("Not enough money to pay.");
+        }
+
+        return newReserve;
     }
 
-    private double priceOfHouse(House house) {
-        return house.getHouseAmenities().stream()
-                .mapToDouble(HouseAmenitie::getPrice)
-                .sum();
-    }
 
     public Reserve findReserveById(Long id) {
         return reserveRepository.findById(id).orElse(new Reserve(-1L));
